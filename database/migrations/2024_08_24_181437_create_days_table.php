d8cf01662044f9757c21d04d31e6d54885f31a18
Polvere243 <danielercoli@gmail.com>
2024-08-26 21:32:46 +0200

    something doesn't work with DB

--- database/migrations/2024_08_24_181437_create_days_table.php
+++ database/migrations/2024_08_24_181437_create_days_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('days', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Travel::class)->nullable()->costrained()->cascadeOnDelete();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
