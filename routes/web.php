3eafd3aba586a86066fe224e07e3babb67533dd2
Polvere243 <danielercoli@gmail.com>
2024-08-27 17:56:04 +0200

    I'm too rusty, I can't get why nothing works and feel sick

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\TravelController as AdminTravelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,17 +27,14 @@ Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', AdminHomeController::class)->name('home');
 
     // rotte per le CRUD dei viaggi
-    Route::get('/travel', [WordController::class, 'index'])->name('travel.index');
-    Route::get('/travel/create', [WordController::class, 'create'])->name('travel.create');
-    Route::get('/travel/{travel}', [WordController::class, 'show'])->name('travel.show')->withTrashed();
-    Route::post('/travel', [WordController::class, 'store'])->name('travel.store');
-    Route::get('/travel/{travel}/edit', [WordController::class, 'edit'])->name('travel.edit')->withTrashed();
-    Route::put('/travel/{travel}', [WordController::class, 'update'])->name('travel.update')->withTrashed();
-    Route::delete('/travel/{travel}', [WordController::class, 'destroy'])->name('travel.destroy');
+    Route::get('/travel', [AdminTravelController::class, 'index'])->name('travel.index');
+    Route::get('/travel/create', [AdminTravelController::class, 'create'])->name('travel.create');
+    Route::get('/travel/{travel}', [AdminTravelController::class, 'show'])->name('travel.show')->withTrashed();
+    Route::post('/travel', [AdminTravelController::class, 'store'])->name('travel.store');
+    Route::get('/travel/{travel}/edit', [AdminTravelController::class, 'edit'])->name('travel.edit')->withTrashed();
+    Route::put('/travel/{travel}', [AdminTravelController::class, 'update'])->name('travel.update')->withTrashed();
+    Route::delete('/travel/{travel}', [AdminTravelController::class, 'destroy'])->name('travel.destroy');
 });
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
