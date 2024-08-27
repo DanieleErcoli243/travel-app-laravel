32c78ca05b879fc5e792298a2dd1d02c9179889d
Polvere243 <danielercoli@gmail.com>
2024-08-27 16:31:52 +0200

    created routes for the CRUD

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,15 @@ Route::get('/', function () {
 Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
     // rotta per la home dell'admin
     Route::get('/', AdminHomeController::class)->name('home');
+
+    // rotte per le CRUD dei viaggi
+    Route::get('/travel', [WordController::class, 'index'])->name('travel.index');
+    Route::get('/travel/create', [WordController::class, 'create'])->name('travel.create');
+    Route::get('/travel/{travel}', [WordController::class, 'show'])->name('travel.show')->withTrashed();
+    Route::post('/travel', [WordController::class, 'store'])->name('travel.store');
+    Route::get('/travel/{travel}/edit', [WordController::class, 'edit'])->name('travel.edit')->withTrashed();
+    Route::put('/travel/{travel}', [WordController::class, 'update'])->name('travel.update')->withTrashed();
+    Route::delete('/travel/{word}', [WordController::class, 'destroy'])->name('travel.destroy');
 });
 
 
